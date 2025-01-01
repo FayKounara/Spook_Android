d6a82dcd7afd186c9331420d5811d4b712d8f2ba
Zoi Gourgioti <117828175+Zoi-Gourgioti@users.noreply.github.com>
2025-01-01 08:37:48 +0200

    Update MainActivity.kt
    Inserts mock data when app starts running

--- app/src/main/java/MainActivity.kt
+++ app/src/main/java/MainActivity.kt
@@ -14,6 +14,9 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        storeViewModel.resetAndInsertMockStores()
+
         setContent {
             RoomDatabaseSetupTheme {
                 HomePageNavigation(storeViewModel, bookingViewModel);
