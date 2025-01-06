629f790ec6096cba70c349859fc3be6bece1ab1e
Zoi Gourgioti <117828175+Zoi-Gourgioti@users.noreply.github.com>
2025-01-06 02:36:30 +0200

    Update OfferDAO.kt

--- app/src/main/java/OfferDAO.kt
+++ app/src/main/java/OfferDAO.kt
@@ -11,6 +11,9 @@ interface OfferDao {
     @Insert
     suspend fun insert(offer: Offer)
 
+    @Query("SELECT * FROM offers_table")
+    fun getAllOffers(): Flow<List<Offer>>
+
     @Query("SELECT * FROM offers_table WHERE storeId = :storeId")
     fun getOffersForStore(storeId: Int): Flow<List<Offer>>
 
