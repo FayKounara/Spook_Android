55c8db09d450654770e96883a22b618254acb1b6
FayKounara <117120486+FayKounara@users.noreply.github.com>
2025-01-14 13:12:14 +0200

    updated BookingViewModel

--- app/src/main/java/BookingViewModel.kt
+++ app/src/main/java/BookingViewModel.kt
@@ -2,9 +2,6 @@ package com.example.room_setup_composables
 
 import android.app.Application
 import android.util.Log
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
@@ -21,54 +18,23 @@ class BookingViewModel(application: Application) : AndroidViewModel(application)
     private val _allBookings = MutableStateFlow<List<Booking>>(emptyList())
     val allBookings: StateFlow<List<Booking>> = _allBookings.asStateFlow()
 
-//    private val _allStores = MutableStateFlow<List<Store>>(emptyList())
-//    val allStores: StateFlow<List<Store>> = _allStores.asStateFlow()
-//
-//    private val _allUsers = MutableStateFlow<List<User>>(emptyList())
-//    val allUsers: StateFlow<List<User>> = _allUsers.asStateFlow()
+    private val _allStores = MutableStateFlow<List<Store>>(emptyList())
+    val allStores: StateFlow<List<Store>> = _allStores.asStateFlow()
 
+    private val _allUsers = MutableStateFlow<List<User>>(emptyList())
+    val allUsers: StateFlow<List<User>> = _allUsers.asStateFlow()
 
-//    private val _selectedStore = MutableStateFlow<Store?>(null)
-//    val selectedStore: StateFlow<Store?> = _selectedStore.asStateFlow()
-//
-//
-//    var enteredStoreId by mutableStateOf("")
-//        private set
+    private val _bookings = MutableStateFlow<List<Booking>>(emptyList())
+    val bookings: StateFlow<List<Booking>> = _bookings
 
     init {
-//        fetchAllBookings()
-//        fetchAllStores()
-//        fetchAllUsers()
+        fetchAllBookings()
+        fetchAllStores()
+        fetchAllUsers()
        // deleteAllBookings()
 
 //        insertDummyUsers()
     }
-    var storeName by mutableStateOf("")
-        private set
-
-    var enteredStoreId by mutableStateOf("")
-        private set
-
-
-
-
-
-    fun fetchStoreNameById(storeId: String) {
-        viewModelScope.launch(Dispatchers.IO) {
-            val name = storeDao.getStoreNameById(storeId)
-            storeName = name ?: "Store Not Found"
-        }
-    }
-
-    fun newEnteredStoreId(storeId: String) {
-        enteredStoreId = storeId
-        fetchStoreNameById(storeId)
-    }
-
-
-
-
-
 
     private fun fetchAllBookings() {
         viewModelScope.launch(Dispatchers.IO) {
@@ -77,28 +43,37 @@ class BookingViewModel(application: Application) : AndroidViewModel(application)
             }
         }
     }
+
+
     private fun deleteAllBookings() {
         viewModelScope.launch(Dispatchers.IO) {
             bookingDao.deleteAllBookings()
         }
     }
 
-//    private fun fetchAllStores() {
-//        viewModelScope.launch(Dispatchers.IO) {
-//            storeDao.getAllStores().collect { stores ->
-//                _allStores.value = stores
-//            }
-//        }
-//    }
-//
-//    private fun fetchAllUsers() {
-//        viewModelScope.launch(Dispatchers.IO) {
-//            userDao.getAllUsers().collect { users ->
-//                _allUsers.value = users
-//            }
-//        }
-//    }
+    private fun fetchAllStores() {
+        viewModelScope.launch(Dispatchers.IO) {
+            storeDao.getAllStores().collect { stores ->
+                _allStores.value = stores
+            }
+        }
+    }
 
+    private fun fetchAllUsers() {
+        viewModelScope.launch(Dispatchers.IO) {
+            userDao.getAllUsers().collect { users ->
+                _allUsers.value = users
+            }
+        }
+    }
+
+    fun getBookingsForUser(userId: Int) {
+        viewModelScope.launch {
+            bookingDao.getBookingsForUser(userId).collect { bookingsList ->
+                _bookings.value = bookingsList
+            }
+        }
+    }
     fun insertBooking(booking: Booking) {
         viewModelScope.launch(Dispatchers.IO) {
             bookingDao.insert(booking)
@@ -113,26 +88,19 @@ class BookingViewModel(application: Application) : AndroidViewModel(application)
         }
     }
 
-//    fun insertStore(store: Store) {
-//        viewModelScope.launch(Dispatchers.IO) {
-//            storeDao.insert(store)
-//            fetchAllStores()
-//        }
-//    }
-//
-//    fun insertUser(user: User) {
-//        viewModelScope.launch(Dispatchers.IO) {
-//            userDao.insert(user)
-//            fetchAllUsers()
-//        }
-//    }
-//
+    fun insertStore(store: Store) {
+        viewModelScope.launch(Dispatchers.IO) {
+            storeDao.insert(store)
+            fetchAllStores()
+        }
+    }
 
-
-//            val name = storeDao.getStoreNameById(storeId)
-//            Log.d("BookingViewModel", "Store name: $name")
-//            storeName = name ?: "Store Not Found"
-
+    fun insertUser(user: User) {
+        viewModelScope.launch(Dispatchers.IO) {
+            userDao.insert(user)
+            fetchAllUsers()
+        }
+    }
 
     fun insertDummyUsers() {
         viewModelScope.launch(Dispatchers.IO) {
