6f71029ce3707b3f0584abe7c4cdd39370c21826
FayKounara <117120486+FayKounara@users.noreply.github.com>
2025-01-14 13:16:34 +0200

    updated userDAO

--- app/src/main/java/UserDAO.kt
+++ app/src/main/java/UserDAO.kt
@@ -14,5 +14,11 @@ interface UserDao {
 
     @Query("SELECT * FROM users_table")
     fun getAllUsers(): Flow<List<User>>
+    // query for profile
+    @Query("SELECT username FROM users_table WHERE userId = :userId")
+    fun getName(userId: Int): String ?
+
+    @Query("SELECT email FROM users_table WHERE userId = :userId")
+    fun getEmail(userId: Int): String ?
 }
 
