4150bf7d0b5e3bf61ae103313b1caa47fe12de41
FayKounara <117120486+FayKounara@users.noreply.github.com>
2025-01-14 15:35:45 +0200

    updated StoreDAO

--- app/src/main/java/StoreDAO.kt
+++ app/src/main/java/StoreDAO.kt
@@ -16,6 +16,12 @@ interface StoreDao {
     @Query("DELETE FROM store_table")
     suspend fun deleteAllStores()
 
-    @Query("Select name from store_table where storeId = :storeId")
-    suspend fun getStoreNameById(storeId: kotlin.String): String
+    @Query("SELECT name FROM store_table WHERE storeId = :storeId")
+    suspend fun getStoreNameById(storeId: Int): String?
+
+    @Query("SELECT location FROM store_table WHERE storeId = :storeId")
+    suspend fun getLocationById(storeId: Int): String?
+
+
+
 }
